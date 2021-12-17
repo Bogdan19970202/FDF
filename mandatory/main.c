accf693a5e2cb0c3680b69cd4420540f8be44918
Bogdan <72202589+Bogdan19970202@users.noreply.github.com>
2021-12-17 14:44:20 +0000

    fdg

--- mandatory/main.c
+++ mandatory/main.c
@@ -47,11 +47,11 @@ int main(void)
   s = buf;
   counter = 0;
   // // z = (char *)ft_realloc(s, ft_strlen(s), 7);
-  printf("%d", fd);
+  // printf("%d", fd);
   
   while (s != NULL)
   {
-    printf("%s", s);
+    // printf("%s", s);
     s = get_next_line(fd);
     if (s == NULL)
       break ;
@@ -60,37 +60,39 @@ int main(void)
     i = 0;
     while (z[i] != NULL)
       i++;
-    printf("%d", i);
+    // printf("\n%d\n", i);
     arr = (t_pix **)malloc(sizeof(t_pix *) * (i + 1));
     
-    if (*arr == NULL)
+    if (arr == NULL)
     {
-      printf("%ssalsm", (char *)(*arr));
+      // printf("%ssalsm", (char *)(*arr));
       return(2);
     }
 
-    // arr[i] = NULL;
+    arr[i] = NULL;
 
-    // i = 0; 
-    // while (z[i] != NULL)
-    // {
-    //   arr[i] = (t_pix *)malloc(sizeof(t_pix));
-    //   if (arr[i] == NULL)
-    //     break ;
-    //   // arr[i]->x = i;
-    //   // arr[i]->y = counter;
-    //   // arr[i]->end = 0;
-    //   // arr[i]->z = *(ft_split(z[i], ',')[0]);
-    //   // if ((ft_split(z[i], ',')[1]) != NULL)
-    //   //   arr[i]->color = *(ft_split(z[i], ',')[0]);
-    //   i++;
-    // }
+    i = 0; 
+    while (z[i] != NULL)
+    {
+      arr[i] = (t_pix *)malloc(sizeof(t_pix));
+      if (arr[i] == NULL)
+        break ;
+      arr[i]->x = i;
+      arr[i]->y = counter;
+      arr[i]->end = 0;
+      arr[i]->z = *(ft_split(z[i], ',')[0]);
+      // if ((ft_split(z[i], ',')[1]) != NULL)
+      //   arr[i]->color = *(ft_split(z[i], ',')[0]);
+      printf("%d\n", arr[i]->z);
+      i++;
+      
+    }
     // arr[i - 1]->end = 1;
     // arr_fin[counter] = arr; 
 
 
     counter ++;
-    printf("%d", counter );
+    // printf("%d", counter );
   }
   // // printf("%d", j );
   // printf("%s", (char *)arr_fin[len] );
